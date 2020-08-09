20d62d428461f9aef890b5a24ea2ae72f28338a8
AJ Slater <aj@slater.net>
2020-08-09 15:07:32 -0700

    dev version if package not installed

--- comicbox/version.py
+++ comicbox/version.py
@@ -4,5 +4,8 @@ import pkg_resources
 
 
 PROGRAM_NAME = "comicbox"
-DISTRIBUTION = pkg_resources.get_distribution(PROGRAM_NAME)
-VERSION = DISTRIBUTION.version
+try:
+    DISTRIBUTION = pkg_resources.get_distribution(PROGRAM_NAME)
+    VERSION = DISTRIBUTION.version
+except pkg_resources.DistributionNotFound:
+    VERSION = "dev"
