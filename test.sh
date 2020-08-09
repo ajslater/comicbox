3de76255c67f794600ef5b70b8315caec42faafb
AJ Slater <aj@slater.net>
2020-08-09 14:20:25 -0700

    try more verbose pytest

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 mkdir -p test-results
-poetry run pytest
+poetry run pytest -vvv
 # pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
 poetry run coverage erase
 poetry run vulture .
