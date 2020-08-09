047c13a68225b40af968b7941af93514703a095b
AJ Slater <aj@slater.net>
2020-08-09 15:18:31 -0700

    unrar install

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
+if [ -n "$(which apt-get)" ]; then
+    sudo apt-get install -y unrar
+elif [ -n "$(which brew)" ]; then
+    brew install unrar
+elif [ -n "$(which pacman)" ]; then
+    pacman -S unrar
+elif [ -n "$(which yum)" ]; then
+    sudo yum install unrar
+else
+    echo "Comicbox requires unrar to open cbr files. Please install it."
+fi
+
 pip3 install poetry
 poetry update
