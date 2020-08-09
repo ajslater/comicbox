54a3eba0df3743d62b683af8d2f28f00ed7dd82a
AJ Slater <aj@slater.net>
2020-08-09 15:26:22 -0700

    try to fix circle ci buseter

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+if [ -n "$CIRCLE_BRANCH" ]; then
+    # Circle buster doesn't have non-free available by default
+    echo 'deb ftp://ftp.us.debian.org/debian/ buster non-free
+deb http://security.debian.org/ buster/updates non-free deb http://volatile.debian.org/debian-volatile buster/volatile non-free' | sudo tee -a /etc/apt/sources.list.d/buster.non-free.list
+    sudo apt-get update
+fi
+
 if [ -n "$(which apt-get)" ]; then
     sudo apt-get install -y unrar
 elif [ -n "$(which brew)" ]; then
