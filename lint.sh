fadd81c72ff6eeb9b7b51c42e1821db35a549a05
AJ Slater <aj@slater.net>
2021-11-21 18:24:45 -0800

    update linting scripts

--- lint.sh
+++ lint.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-set -euo pipefail
+set -euxo pipefail
+if [ "${1:-}" = "-f" ]; then
+    # Fix before check
+    ./fix-lint.sh
+fi
 poetry run flake8 .
 poetry run isort --check-only --color .
 poetry run black --check .
 poetry run pyright .
 poetry run vulture .
 prettier --check .
-shellcheck -x ./*.sh ./ci/*.sh
+shellcheck -x ./*.sh ./**/*.sh
+shfmt -d -i 4 ./*.sh ./**/*.sh
 # hadolint build.Dockerfile
