8c53318ac30aef15c64b2d6069c91ac436096a41
AJ Slater <aj@slater.net>
2022-04-19 15:45:52 -0700

    update builder base and streamline build process

--- docker/docker-compose-exit.sh
+++ docker/docker-compose-exit.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Run a docker compose service and return its exit code
 set -euo pipefail
-source .env.build
-export DOCKER_CLI_EXPERIMENTAL=enabled
-export DOCKER_BUILDKIT=1
+SERVICE=$1
 # docker compose without the dash doesn't have the exit-code-from param
-docker-compose up --exit-code-from "$1" "$1"
+docker compose up --exit-code-from "$SERVICE" "$SERVICE"
