7470e1f7f6f0aa15920380ce7336d4ee78a26459
AJ Slater <aj@slater.net>
2025-01-23 14:38:53 -0800

    format

--- comicbox/box/metadata.py
+++ comicbox/box/metadata.py
@@ -53,8 +53,8 @@ class ComicboxMetadataMixin(ComicboxComputedMixin):
     ) -> dict:
         """Get merged metadata as a dict."""
         schema, md = self._to_dict(transform_class)
-        dump =  schema.dump(md, **kwargs)
-        return dict(dump) # type:ignore[reportArgumentType]
+        dump = schema.dump(md, **kwargs)
+        return dict(dump)  # type:ignore[reportArgumentType]
 
     @archive_close
     def to_string(
