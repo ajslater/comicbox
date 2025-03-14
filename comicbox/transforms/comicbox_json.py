1d916d5e5ab631a31278dfb9dc86423725e9b3f6
AJ Slater <aj@slater.net>
2025-03-13 18:52:45 -0700

    remove comicbox_mixin transform. empty

--- comicbox/transforms/comicbox_json.py
+++ comicbox/transforms/comicbox_json.py
@@ -1,11 +1,14 @@
 """Comicbox JSON transform to and from Comicbox format."""
 
+from bidict import frozenbidict
+
 from comicbox.schemas.comicbox_json import ComicboxJsonSchema
-from comicbox.transforms.comicbox_mixin import ComicboxTransformMixin
+from comicbox.schemas.comicbox_mixin import APP_ID_KEY, TAGGER_KEY
 from comicbox.transforms.json_transforms import JsonTransform
 
 
-class ComicboxJsonTransform(ComicboxTransformMixin, JsonTransform):
+class ComicboxJsonTransform(JsonTransform):
     """Comicbox JSON transform."""
 
     SCHEMA_CLASS = ComicboxJsonSchema
+    TOP_TAG_MAP = frozenbidict({(APP_ID_KEY, None): (TAGGER_KEY, None)})
