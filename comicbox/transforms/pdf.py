9abc8ef04969ce0c1fb4208a10ccd3709e3fea7a
AJ Slater <aj@slater.net>
2025-03-17 13:25:23 -0700

    glom for pdf credits

--- comicbox/transforms/pdf.py
+++ comicbox/transforms/pdf.py
@@ -6,15 +6,11 @@ from comicbox.schemas.comet import CoMetRoleTagEnum
 from comicbox.schemas.comicbox_mixin import (
     CREDITS_KEY,
     GENRES_KEY,
-    IMPRINT_KEY,
-    ISSUE_KEY,
-    PUBLISHER_KEY,
     ROLES_KEY,
     SCAN_INFO_KEY,
-    SERIES_KEY,
     TAGGER_KEY,
+    TAGS_KEY,
     UPDATED_AT_KEY,
-    VOLUME_KEY,
 )
 from comicbox.schemas.comicinfo_enum import ComicInfoRoleTagEnum
 from comicbox.schemas.metroninfo_enum import MetronRoleEnum
@@ -29,12 +25,55 @@ from comicbox.transforms.xml_transforms import XmlTransform
 
 LOG = getLogger(__name__)
 
+_AUTHOR_VALUES = frozenset(
+    {
+        enum.value
+        for enum in (
+            GenericRoleEnum.AUTHOR,
+            MetronRoleEnum.WRITER,
+            MetronRoleEnum.SCRIPT,
+            MetronRoleEnum.STORY,
+            MetronRoleEnum.PLOT,
+            MetronRoleEnum.TRANSLATOR,
+            CoMetRoleTagEnum.CREATOR,
+            CoMetRoleTagEnum.WRITER,
+            ComicInfoRoleTagEnum.WRITER,
+            ComicInfoRoleTagEnum.TRANSLATOR,
+        )
+    }
+    | {*GenericRoleAliases.WRITER.value}
+)
+
+
+def _authors_to_credits(authors):
+    return {author: {ROLES_KEY: {"Writer": {}}} for author in authors if author}
+
+
+def _credits_to_authors(comicbox_credits):
+    authors = set()
+    for person_name, comicbox_credit in comicbox_credits.items():
+        if not person_name:
+            continue
+        comicbox_roles = comicbox_credit.get(ROLES_KEY, {})
+        for role_name in comicbox_roles:
+            if role_name.lower() in _AUTHOR_VALUES:
+                authors.add(person_name)
+    return authors
+
+
+def authors_to_credits_transform(author_tag):
+    """Create a Transform for pdf authors to comicbox credits."""
+    return KeyTransforms(
+        key_map={author_tag: CREDITS_KEY},
+        to_cb=_authors_to_credits,
+        from_cb=_credits_to_authors,
+    )
+
 
 class PDFXmlTransform(XmlTransform):
     """PDF Schema."""
 
     SCHEMA_CLASS = PDFXmlSchema
-    AUTHOR_TAG = "pdf:Author"
     TRANSFORM_MAP = create_transform_map(
         KeyTransforms(
             key_map={
@@ -43,88 +82,28 @@ class PDFXmlTransform(XmlTransform):
                 "pdf:Producer": TAGGER_KEY,
                 "pdf:ModDate": UPDATED_AT_KEY,
                 # "pdf:Title": CODED
-                **{key: key for key in ("credits", "tags")},
-                **{key: key for key in ("pdf:Author", "pdf:Keywords")},
+                # TODO fix normally for regular keywords and embedded
+                **{key: key for key in ("pdf:Keywords", TAGS_KEY)},
             }
         ),
+        authors_to_credits_transform("pdf:Author"),
         name_obj_to_string_list_key_transforms(
             {
-                # TAGS_TAG: CODED EMBEDDED
                 "pdf:Subject": GENRES_KEY,
             },
         ),
         stories_key_transform("pdf:Title"),
     )
-    TAGS_TAG = "pdf:Keywords"
-    GROUP_KEYS = frozenset(
-        {PUBLISHER_KEY, IMPRINT_KEY, SERIES_KEY, VOLUME_KEY, ISSUE_KEY}
-    )
-    GROUP_TAG_DELIMETER = ":"
-    TITLE_STORIES_DELIMITER = ";"
-    AUTHOR_VALUES = frozenset(
-        {
-            enum.value
-            for enum in (
-                GenericRoleEnum.AUTHOR,
-                MetronRoleEnum.WRITER,
-                MetronRoleEnum.SCRIPT,
-                MetronRoleEnum.STORY,
-                MetronRoleEnum.PLOT,
-                MetronRoleEnum.TRANSLATOR,
-                CoMetRoleTagEnum.CREATOR,
-                CoMetRoleTagEnum.WRITER,
-                ComicInfoRoleTagEnum.WRITER,
-                ComicInfoRoleTagEnum.TRANSLATOR,
-            )
-        }
-        | {*GenericRoleAliases.WRITER.value}
-    )
 
-    def parse_credits(self, data):
-        """Convert csv to writer credits."""
-        authors = data.get(self.AUTHOR_TAG)
-        if not authors:
-            return data
-        comicbox_credits = {
-            author: {ROLES_KEY: {"Writer": {}}} for author in authors if author
-        }
-        if comicbox_credits:
-            data[CREDITS_KEY] = comicbox_credits
-        return data
+    TO_COMICBOX_PRE_TRANSFORM = (*XmlTransform.TO_COMICBOX_PRE_TRANSFORM,)
 
-    def unparse_credits(self, data):
-        """Convert writer credits to csv."""
-        comicbox_credits = data.pop(CREDITS_KEY, {})
-        authors = set()
-        for person_name, comicbox_credit in comicbox_credits.items():
-            if not person_name:
-                continue
-            comicbox_roles = comicbox_credit.get(ROLES_KEY, {})
-            for role_name in comicbox_roles:
-                if role_name.lower() in self.AUTHOR_VALUES:
-                    authors.add(person_name)
-        if authors:
-            data[self.AUTHOR_TAG] = authors
-        return data
-
-    TO_COMICBOX_PRE_TRANSFORM = (
-        *XmlTransform.TO_COMICBOX_PRE_TRANSFORM,
-        parse_credits,
-    )
-
-    FROM_COMICBOX_PRE_TRANSFORM = (
-        *XmlTransform.FROM_COMICBOX_PRE_TRANSFORM,
-        unparse_credits,
-    )
+    FROM_COMICBOX_PRE_TRANSFORM = (*XmlTransform.FROM_COMICBOX_PRE_TRANSFORM,)
 
 
 class MuPDFTransform(PDFXmlTransform):
     """MuPDF Transformer."""
 
     SCHEMA_CLASS = MuPDFSchema
-    AUTHOR_TAG = "author"
-    TITLE_TAG = "title"
-    TAGS_TAG = "keywords"
     TRANSFORM_MAP = create_transform_map(
         KeyTransforms(
             key_map={
@@ -133,13 +112,13 @@ class MuPDFTransform(PDFXmlTransform):
                 "modDate": UPDATED_AT_KEY,
                 "producer": TAGGER_KEY,
                 # "title": CODED
-                **{key: key for key in ("credits", "tags")},
-                **{key: key for key in ("author", "keywords")},
+                # TODO fix normally for regular keywords and embedded
+                **{key: key for key in ("keywords", TAGS_KEY)},
             }
         ),
+        authors_to_credits_transform("author"),
         name_obj_to_string_list_key_transforms(
             {
-                # "keywords": CODED, EMBEDDED
                 "subject": GENRES_KEY,
             },
         ),
