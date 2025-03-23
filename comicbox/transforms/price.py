c4b48a15c45d21964ed89af6312b3a97f5ada380
AJ Slater <aj@slater.net>
2025-03-22 23:59:01 -0700

    remove comments

--- comicbox/transforms/price.py
+++ comicbox/transforms/price.py
@@ -6,13 +6,11 @@ from comicbox.transforms.transform_map import KeyTransforms
 
 def price_to_obj(_source_data, price):
     """Price to a language keyed price object."""
-    # TODO get language from data
     return {"": price} if price is not None else None
 
 
 def obj_to_price(_source_data, comicbox_prices):
     """Return first price."""
-    # TODO match price to language
     return next(iter(comicbox_prices.values())) if comicbox_prices else None
 
 
