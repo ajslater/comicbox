c44bff1977c6ce26c65449aa83efbe691f55520a
AJ Slater <aj@slater.net>
2025-04-04 12:47:16 -0700

    glomify stories transform

--- comicbox/transforms/stories.py
+++ comicbox/transforms/stories.py
@@ -1,30 +1,28 @@
 """Title to Stories Transform Mixin."""
 
+from glom import SKIP, Check, Coalesce, Invoke, T, Val
+from glom.grouping import Group
+
 from comicbox.schemas.comicbox_mixin import STORIES_KEY
 from comicbox.transforms.spec import MetaSpec
 
 TITLE_STORIES_DELIMITER = ";"
 
 
-def title_to_stories(title):
-    """Split titles into stories."""
-    names = title.split(TITLE_STORIES_DELIMITER)
-    return {
-        stripped_name: {} for name in names if name and (stripped_name := name.strip())
-    }
-
-
-def stories_to_title(stories):
-    """Join stories into a title."""
-    names = [story for story in stories if story]
-    return TITLE_STORIES_DELIMITER.join(names)
-
-
 def stories_key_transform_to_cb(title_tag):
     """Create a key transformer for a title tag."""
     return MetaSpec(
         key_map={STORIES_KEY: title_tag},
-        spec=title_to_stories,
+        spec=(
+            Invoke(T.split).constants(TITLE_STORIES_DELIMITER),
+            Group(
+                {
+                    Coalesce(
+                        (Invoke(T.strip)), skip=lambda x: not x, default=SKIP
+                    ): Val({})
+                }
+            ),
+        ),
     )
 
 
@@ -32,5 +30,5 @@ def stories_key_transform_from_cb(title_tag):
     """Create a key transformer for a title tag."""
     return MetaSpec(
         key_map={title_tag: STORIES_KEY},
-        spec=stories_to_title,
+        spec=([Check(validate=bool)], Invoke(TITLE_STORIES_DELIMITER.join).specs(T)),
     )
