f2b11c45ae0541f2a18729246625f794c400b3a5
AJ Slater <aj@slater.net>
2025-04-08 11:52:40 -0700

    format

--- comicbox/schemas/metroninfo/identifiers.py
+++ comicbox/schemas/metroninfo/identifiers.py
@@ -4,10 +4,7 @@ from types import MappingProxyType
 
 from comicbox.fields.fields import StringField
 from comicbox.fields.metroninfo import MetronIDAttrField, MetronSourceField
-from comicbox.fields.xml_fields import (
-    XmlBooleanAttributeField,
-    XmlStringField,
-)
+from comicbox.fields.xml_fields import XmlBooleanAttributeField, XmlStringField
 from comicbox.schemas.xml_schemas import XmlSubSchema
 
 
