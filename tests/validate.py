eefacfd25acec363e0cda889f944ca2bef83ea5b
AJ Slater <aj@slater.net>
2025-04-08 17:03:15 -0700

    Use filesystem loader for validating jsonschemas so it resolves

--- tests/validate.py
+++ tests/validate.py
@@ -4,46 +4,60 @@ from pathlib import Path
 from types import MappingProxyType
 
 import simplejson
-from jsonschema import validate as jsonschema_validate
+from jsonschema import Draft202012Validator
+from referencing import Registry, Resource
 from ruamel.yaml import YAML
 from xmlschema import XMLSchema11
+
+SCHEMAS_PATH = Path("schemas")
 
 
 def get_xml_schema(path):
     """Get the xml11 schema."""
+    path = SCHEMAS_PATH / path
     return XMLSchema11(Path(path))
+
+
+SCHEMA_ROOT = Path(__file__).parent.parent / "schemas/v2.0"
+SCHEMA_ID = "https://github.com/ajslater/comicbox/blob/main/schemas/v2.0/"
+
+
+def _retrieve_from_filesystem(uri: str):
+    """Resolve local $refs instead of trying the uri for development."""
+    # https://python-jsonschema.readthedocs.io/en/latest/referencing/
+    path = SCHEMA_ROOT / Path(uri.removeprefix(SCHEMA_ID))
+    contents = simplejson.loads(path.read_text())
+    return Resource.from_contents(contents)
+
+
+REGISTRY = Registry(retrieve=_retrieve_from_filesystem)
 
 
 def get_json_schema(path):
     """Get the jsonschema."""
+    path = SCHEMAS_PATH / path
     with Path(path).open("r") as f:
         schema_str = f.read()
-    return simplejson.loads(schema_str)
+    json_schema = simplejson.loads(schema_str)
+    return Draft202012Validator(json_schema, registry=REGISTRY)
 
 
-CBI_SCHEMA = get_json_schema("schemas/comic-book-info-v1.0.schema.json")
-CB_SCHEMA = get_json_schema("schemas/v2.0/comicbox-v2.0.schema.json")
-MIX_SCHEMA = get_xml_schema("schemas/MetronInfo-v1.0.xsd")
-CIX_SCHEMA = get_xml_schema("schemas/ComicInfo-v2.1-Draft.xsd")
-COMET_SCHEMA = get_xml_schema("schemas/CoMet-v1.1.xsd")
-
+CB_SCHEMA = get_json_schema("v2.0/comicbox-v2.0.schema.json")
 FMT_SCHEMA_MAP = MappingProxyType(
     {
-        "cix": CIX_SCHEMA,
-        "cbi": CBI_SCHEMA,
-        "metron": MIX_SCHEMA,
-        "comet": COMET_SCHEMA,
+        "cix": get_xml_schema("ComicInfo-v2.1-Draft.xsd"),
+        "cbi": get_json_schema("comic-book-info-v1.0.schema.json"),
+        "metron": get_xml_schema("MetronInfo-v1.0.xsd"),
+        "comet": get_xml_schema("CoMet-v1.1.xsd"),
         "json": CB_SCHEMA,
         "yaml": CB_SCHEMA,
     }
 )
-
-
 DEFAULT_FORMAT_MAP = MappingProxyType(
     {
-        # "comicbox.json": "json",
-        # "comicbox.yaml": "yaml",
-        # "comicbox-cli.yaml": "yaml"
+        "comicbox.json": "json",
+        "comicbox.yaml": "yaml",
+        "comicbox-cli.yaml": "yaml",
         "comicinfo.xml": "cix",
         "comic-book-info.json": "cbi",
         "metroninfo.xml": "metron",
@@ -51,9 +65,6 @@ DEFAULT_FORMAT_MAP = MappingProxyType(
         # "comictagger.json": "no-validation",
         # "mupdf.json": "no-validation",
         # "pdf-metadata.xml": "no-validation",
-        "comicbox.json": "temp-autopass",
-        "comicbox.yaml": "temp-autopass",
-        "comicbox-cli.yaml": "temp-autopass",
     }
 )
 
@@ -65,11 +76,10 @@ def validate_path(path, fmt=None):
     if not fmt or fmt == "temp-autopass":
         return
     path = Path(path)
-    schema = FMT_SCHEMA_MAP[fmt]
+    validator = FMT_SCHEMA_MAP[fmt]
     suffix = path.suffix
-    if suffix == ".xml":
-        xml_schema: XMLSchema11 = schema  # type: ignore[reportAssignmentType]
-        xml_schema.validate(path)
+    if isinstance(validator, XMLSchema11):
+        validator.validate(path)
     else:
         print(path)
         with Path(path).open("r") as f:
@@ -81,5 +91,4 @@ def validate_path(path, fmt=None):
         else:
             reason = f"Bad suffix for validation: {suffix} : {path}"
             raise ValueError(reason)
-        json_schema: dict = schema  # type: ignore[reportAssignmentType]
-        jsonschema_validate(instance=md_dict, schema=json_schema)
+        validator.is_valid(md_dict)
