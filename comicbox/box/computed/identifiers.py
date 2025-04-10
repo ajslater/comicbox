b9bc5cc97830e1b524ec529ca595e692d3c5f787
AJ Slater <aj@slater.net>
2025-04-10 14:46:49 -0700

    remove idps compute. unused

--- comicbox/box/computed/identifiers.py
+++ comicbox/box/computed/identifiers.py
@@ -7,7 +7,6 @@ from comicbox.formats import MetadataFormats
 from comicbox.identifiers.const import (
     ALIAS_NID_MAP,
     DEFAULT_NID,
-    NIDs,
 )
 from comicbox.identifiers.identifiers import (
     create_identifier,
@@ -20,15 +19,10 @@ from comicbox.identifiers.urns import (
 )
 from comicbox.merge import AdditiveMerger
 from comicbox.schemas.comicbox import (
-    IDENTIFIER_PRIMARY_SOURCE_KEY,
     IDENTIFIERS_KEY,
     TAGS_KEY,
 )
-from comicbox.transforms.identifiers import create_identifier_primary_source
 
-_FORMATS_WITH_IPS = frozenset(
-    {MetadataFormats.COMICTAGGER, MetadataFormats.METRON_INFO}
-)
 _FORMATS_WITH_TAGS_WITHOUT_IDS = frozenset(
     {
         MetadataFormats.COMIC_BOOK_INFO,
@@ -38,13 +32,13 @@ _FORMATS_WITH_TAGS_WITHOUT_IDS = frozenset(
         MetadataFormats.PDF_XML,
     }
 )
-_IDPS_KEYS = frozenset({IDENTIFIERS_KEY, IDENTIFIER_PRIMARY_SOURCE_KEY})
 
 
 class ComicboxComputedIdentifers(ComicboxComputedIssue):
     """Comicbox computed identifiers."""
 
     def _get_computed_from_tags(self, sub_data):
+        # only look for ids in tags if the format has tags but no designated id field.
         if (
             not sub_data
             or TAGS_KEY in self._config.delete_keys
@@ -68,30 +62,9 @@ class ComicboxComputedIdentifers(ComicboxComputedIssue):
             return None
         return {IDENTIFIERS_KEY: identifiers}
 
-    def _get_computed_from_identifiers(self, sub_data):
-        if (
-            frozenset(self._config.delete_keys) & _IDPS_KEYS
-            or not (self._config.write & _FORMATS_WITH_IPS)
-            or not sub_data
-            or sub_data.get(IDENTIFIER_PRIMARY_SOURCE_KEY)
-        ):
-            return None
-        identifiers = sub_data.get(IDENTIFIERS_KEY)
-        if not identifiers:
-            return None
-        result = None
-        for nid in NIDs:
-            if nid.value in identifiers and (
-                ips := create_identifier_primary_source(nid)
-            ):
-                result = {IDENTIFIER_PRIMARY_SOURCE_KEY: ips}
-                break
-        return result
-
     COMPUTED_ACTIONS = MappingProxyType(
         {
             **ComicboxComputedIssue.COMPUTED_ACTIONS,
             "from tags": (_get_computed_from_tags, AdditiveMerger),
-            "from identifiers": (_get_computed_from_identifiers, AdditiveMerger),
         }
     )
