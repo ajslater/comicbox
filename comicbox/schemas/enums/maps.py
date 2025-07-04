87ba2b2ec8a886f5e389e1a6764d53107e1a3e64
AJ Slater <aj@slater.net>
2025-07-03 18:10:04 -0700

    more uniformity in generic original formats. translate original format variants to metron

--- comicbox/schemas/enums/maps.py
+++ comicbox/schemas/enums/maps.py
@@ -125,29 +125,76 @@ METRON_AGE_RATING_MAP: MappingProxyType[Enum, Enum] = MappingProxyType(
     }
 )
 
+
+def _variants(enum, *, dashed_canon=False):
+    space = " " if not dashed_canon else "-"
+    dash = "-" if not dashed_canon else " "
+    return {
+        enum.value.replace(space, dash): enum,
+        enum.value.replace(space, ""): enum,
+        enum.value.replace("'", ""): enum,
+        enum.value.replace("'", "").replace(space, dash): enum,
+    }
+
+
+def _all_variants(enums):
+    variant_map = {}
+    for enum in enums:
+        variants = _variants(enum)
+        variant_map.update(variants)
+    return variant_map
+
+
+GENERIC_FORMAT_MAP: MappingProxyType[Enum | str, Enum] = MappingProxyType(
+    {
+        **_all_variants(GenericFormatEnum),
+        "Boxed Set": GenericFormatEnum.BOX_SET,
+        "GN": GenericFormatEnum.GRAPHIC_NOVEL,
+        "HC": GenericFormatEnum.HARD_COVER,
+        **_variants(GenericFormatEnum.ONE_SHOT, dashed_canon=True),
+        "TBP": GenericFormatEnum.TRADE_PAPERBACK,
+        "TP": GenericFormatEnum.TRADE_PAPERBACK,
+    }
+)
+
+
+def _translate_generic_to(generic_enum, metron_enum):
+    multi_map = {}
+    for key, value in GENERIC_FORMAT_MAP.items():
+        if value == generic_enum:
+            multi_map[key] = metron_enum
+
+    return multi_map
+
+
 METRON_FORMAT_MAP: MappingProxyType[Enum, Enum] = MappingProxyType(
     {
         # GenericFormatEnum.ANTHOLOGY: MetronFormatEnum.,
         # GenericFormatEnum.ANNOTATION: MetronFormatEnum.,
-        GenericFormatEnum.BOX_SET: MetronFormatEnum.OMNIBUS,
+        **_translate_generic_to(GenericFormatEnum.BOX_SET, MetronFormatEnum.OMNIBUS),
         GenericFormatEnum.DIGITAL: MetronFormatEnum.DIGITAL_CHAPTER,
         # GenericFormatEnum.DIRECTORS_CUT: MetronFormatEnum.,
         # GenericFormatEnum.DIRECTOR_S_CUT: MetronFormatEnum.,
         GenericFormatEnum.GIANT_SIZED: MetronFormatEnum.ANNUAL,
-        GenericFormatEnum.GN: MetronFormatEnum.GRAPHIC_NOVEL,
-        GenericFormatEnum.HARD_COVER: MetronFormatEnum.HARDCOVER,
-        GenericFormatEnum.HC: MetronFormatEnum.HARDCOVER,
+        **_translate_generic_to(
+            GenericFormatEnum.GRAPHIC_NOVEL, MetronFormatEnum.GRAPHIC_NOVEL
+        ),
+        **_translate_generic_to(
+            GenericFormatEnum.HARD_COVER, MetronFormatEnum.HARDCOVER
+        ),
         GenericFormatEnum.HD_UPSCALED: MetronFormatEnum.DIGITAL_CHAPTER,
-        GenericFormatEnum.KING_SIZED: MetronFormatEnum.ANNUAL,
+        **_translate_generic_to(GenericFormatEnum.KING_SIZED, MetronFormatEnum.ANNUAL),
         # GenericFormatEnum.MAGAZINE: MetronFormatEnum.,
         # GenericFormatEnum.MANGA: MetronFormatEnum.,
-        GenericFormatEnum.ONE_DASH_SHOT: MetronFormatEnum.ONE_SHOT,
+        **_translate_generic_to(GenericFormatEnum.ONE_SHOT, MetronFormatEnum.ONE_SHOT),
         # GenericFormatEnum.PDF_RIP: MetronFormatEnum.,
         # GenericFormatEnum.PREVIEW: MetronFormatEnum.,
         # GenericFormatEnum.PROLOGUE: MetronFormatEnum.,
         # GenericFormatEnum.SCANLATION: MetronFormatEnum.,
         # GenericFormatEnum.SCRIPT: MetronFormatEnum.,
-        GenericFormatEnum.TBP: MetronFormatEnum.TRADE_PAPERBACK,
+        **_translate_generic_to(
+            GenericFormatEnum.TRADE_PAPERBACK, MetronFormatEnum.TRADE_PAPERBACK
+        ),
         # GenericFormatsEnum.WEB_COMIC: MetronFormatEnum.,
         # GenericFormatsEnum.WEB_RIP: MetronFormatEnum.,
     }
