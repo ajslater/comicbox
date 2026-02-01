1e7b103c7b435db2e487c13f6415d5ca62ba6c87
AJ Slater <aj@slater.net>
2026-01-31 18:08:05 -0800

    fix test script

--- bin/test.sh
+++ bin/test.sh
@@ -2,6 +2,6 @@
 # Run all tests
 set -euxo pipefail
 mkdir -p test-results
-LOGLEVEL=DEBUG uv --group test run pytest "$@"
+LOGLEVEL=DEBUG uv run --group test run pytest "$@"
 # pytest-cov leaves .coverage.$HOST.$PID.$RAND files around while coverage itself doesn't
 uv run --group test coverage erase || true
